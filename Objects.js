d903ef1c60c5d7634bbe85630b94858d3a10e5fe
Rameesha Khan <rameeshakhan157@gmail.com>
2024-02-18 22:48:36 +0500

    Objects Added

---
+++ Objects.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "Rameesha",
+    email: 'ram@gmail.com',
+    isActive: false
+};
+var reactCourse = {
+    name: 'React V1',
+    isPaid: true,
+    amount: 2000
+};
+// taking object arg , returning object , we wont write object , instead we use symbol for object that is {}
+function createUser(User) {
+    return User;
+}
+createUser(User);
+// return an object which should have always and always these defined properties
+function createCourse(arg) {
+    return {
+        name: arg.name,
+        isPaid: arg.isPaid
+        // amount: arg.amount  // -> Cannot return any other attribute that is not defined but ref to line 32
+    };
+}
+createCourse({ name: "rameesha", isPaid: false, amount: 3000 });
+createCourse(reactCourse);
