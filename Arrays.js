1e3432e9be9c1c37743c01b6248a36387b8fd7a0
Rameesha Khan <rameeshakhan157@gmail.com>
2024-02-19 12:52:58 +0500

    Arrays In TypeScript

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [3];
+superHeros.push(9);
+superHeros.push(2);
+superHeros.push(4);
+superHeros.push(5);
+console.log(superHeros);
---
+++ Arrays.ts
@@ -0,0 +1,30 @@
+
+const superHeros : number[] = [];
+
+const num1 : Array<number> = [] // another syntax 
+
+// Array of Arrays -> 2d Array 
+const matrix: number[][] = [
+    [255,255,255],
+    [0,0,0]
+]
+
+superHeros.push(9);
+superHeros.push(2);
+superHeros.push(4);
+superHeros.push(5);
+
+console.log(superHeros);
+
+export {};
+
+num1.push(4)
+
+type obj = {
+    name : string
+    isActive : boolean
+}
+
+const userList: obj[] = []
+
+userList.push({name: "" , isActive : true})
